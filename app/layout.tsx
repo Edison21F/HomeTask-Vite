c89248db5828f4ce22fc484f33dae3bfae3931e7
Edison21F <leoardoedi2004@outlook.com>
2025-02-12 11:24:02 -0500

    analisis

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import type React from "react" // Added import for React
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -17,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      <Analytics/>
       <body className={inter.className}>{children}</body>
     </html>
   )
