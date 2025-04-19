f8cfff337f5d24bd62fba2f7ea99308355880c2b
Edison21F <leonardoedi2004@outlook.com>
2025-04-19 11:12:17 -0500

    task

--- src/pages/TasksPage.jsx
+++ src/pages/TasksPage.jsx
@@ -5,6 +5,8 @@ function TaskPage() {
     return (
         <div className="container mx-auto px-4 py-8">
             <h1>Task Page</h1>
+
+            <p>si lee esto es gay JAJAJAJJA</p>
         </div>
     );
 }
