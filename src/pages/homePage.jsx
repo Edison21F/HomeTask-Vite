66141c3cdeae5fe06e0f50f89208e7ad6883728d
Edison21F <leonardoedi2004@outlook.com>
2025-04-19 11:25:31 -0500

    register

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,6 +2,9 @@ function HomePage() {
     return (
         <div >
             <h1>Home Page</h1>
+            {/* botones para dirigir login o register  */}
+            <button onClick={() => navigate("/login")}>Login</button>
+            <button onClick={() => navigate("/register")}>Register</button>
         </div>
     );
 }
